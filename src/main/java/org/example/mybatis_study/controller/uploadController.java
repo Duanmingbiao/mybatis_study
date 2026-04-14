f84b1e430d5f7cc8fb46603a1a3cc6d964d4a80a
duan. <3102739708@qq.com>
2026-04-14 23:16:39 +0800

    上传本地文件

---
+++ src/main/java/org/example/mybatis_study/controller/uploadController.java
@@ -0,0 +1,28 @@
+package org.example.mybatis_study.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.mybatis_study.pojo.Result;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping
+@Slf4j
+public class uploadController {
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        int i = fileName.lastIndexOf(".");
+        String suffix = fileName.substring(i);
+        String newFileName = UUID.randomUUID().toString() + "." + suffix;
+        file.transferTo(new File("E:\\test\\" + newFileName));
+        return Result.success();
+    }
+}
